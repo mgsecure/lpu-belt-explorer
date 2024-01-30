214375836aa437f0e6cf7c2c5073d1b5613cfc8b
mgsecure <mgsecure@users.noreply.github.com>
2024-01-30 11:07:42 +0000

    deploy: 6477ae4b099377855b89d9637eba26f465b83bc2

--- assets/PrivacyRoute-fa2951cb.js
+++ assets/PrivacyRoute-fa2951cb.js
@@ -1,4 +1,4 @@
-import{j as e,R as o,K as t,N as n,ak as a,al as r}from"./index-2247e2dd.js";import{R as i}from"./react-markdown-76119197.js";import"./index-5a581687.js";const s=`# Privacy Policy
+import{j as e,R as o,K as t,N as n,ak as a,al as r}from"./index-a649afa8.js";import{R as i}from"./react-markdown-10f43a8e.js";import"./index-62114279.js";const s=`# Privacy Policy
 
 Last updated: November 28, 2023
 
